eebfb9436b6f4564edb8f14dd45806d328656e8c
NyamK <okkao1825@gmail.com>
2022-10-24 12:52:19 +0800

    starting

---
+++ qs/index.js
@@ -0,0 +1,14 @@
+const qs = require('qs')
+
+const main = () => {
+  const obj = {
+    type: '',
+    query: '',
+    nearLat: '',
+    equipments: ['1', '2']
+  }
+
+  console.log(qs.stringify(obj))
+}
+
+main()
