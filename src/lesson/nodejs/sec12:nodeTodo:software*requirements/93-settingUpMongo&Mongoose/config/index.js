c49de337b8634adb4f0f55f81063d0316609302c
NyamK <okkao1825@gmail.com>
2023-02-19 17:42:45 +0800

    added:character-converter && email-masker

--- src/lesson/nodejs/sec12:nodeTodo:software*requirements/93-settingUpMongo&Mongoose/config/index.js
+++ src/lesson/nodejs/sec12:nodeTodo:software*requirements/93-settingUpMongo&Mongoose/config/index.js
@@ -1,7 +1,10 @@
 var configValues = require("./config");
 
+const username = 'nya3mk'
+const password = 'N...k2..9'
+
 module.exports = {
 	getDbConnectionString: function () {
-		return "mongodb://nyaaamk:Nyamk2929@cluster0.uvef8au.mongodb.net/test";
+		return `mongodb://${username}:${password}@cluster0.uvef8au.mongodb.net/test`;
 	}
 };
---
+++ src/utils/email-masker.js
@@ -0,0 +1,14 @@
+const str = "okkao1825@gmail.com";
+
+const maskEmail = (mail) => {
+	const [email, domain] = mail.split("@");
+
+	return `${email.split("").map((char, index) => {
+			if (index === 0 || index === email.length - 1) {
+				return char;
+			}
+			return "*";
+		}).join("")}@${domain}`;
+};
+
+console.log(maskEmail(str));
