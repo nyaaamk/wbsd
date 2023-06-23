e8d35c4425ccbf42115b952fd7e259f4bf385396
NyamK <okkao1825@gmail.com>
2023-06-23 19:40:39 +0800

    updates: on character-converter

--- src/regex/index.js
+++ src/regex/index.js
@@ -1,8 +1,8 @@
 // whiteSpace \s
 // not allow space \S
 
-const removeAllWhiteSpace = replace(/ /g, '');
+const removeAllWhiteSpace = replace(/ /g, "");
 
-const mglString = /^[А-Яа-я|Үү|Өө|Ёё]{2,120}$/
+const mglString = /^[А-Яа-я|Үү|Өө|Ёё]{2,120}$/;
 
-const engString = /^[A-Za-z]{2,120}$/
+const engString = /^[A-Za-z]{2,120}$/;
