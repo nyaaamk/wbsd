f2d26027c7454d8c88c5bbc97b7bb3760c80af93
NyamK <okkao1825@gmail.com>
2023-10-09 09:40:37 +0800

    updates: on regex

--- src/regex/index.js
+++ src/regex/index.js
@@ -1,10 +1,16 @@
 // whiteSpace \s
 // not allow space \S
 
-const removeAllWhiteSpace = replace(/ /g, "");
+export const removeAllWhiteSpace = replace(/ /g, "");
 
-const mglString = /^[А-Яа-я|Үү|Өө|Ёё]{2,120}$/;
+export const parsedSTring = "   John     Doe    ".replace(/ +/g, " ").trim();
 
-const engString = /^[A-Za-z]{2,120}$/;
+export const mglString = /^[А-Яа-я|Үү|Өө|Ёё|\s|.]{2,255}$/;
 
-const phone = /^[(9|8|6]{1}[0-9]{7}$/;
+export const engString = /^[A-Za-z]{2,120}$/;
+
+export const phone = /^[(9|8|6]{1}[0-9]{7}$/;
+
+export const strongPassword = /^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})$/;
+
+export const optionalPassword = /^((?=.*[\d])(?=.*[a-z])(?=.*[A-Z])|(?=.*[a-z])(?=.*[A-Z])(?=.*[^\w\d\s])|(?=.*[\d])(?=.*[A-Z])(?=.*[^\w\d\s])|(?=.*[\d])(?=.*[a-z])(?=.*[^\w\d\s])).{7,30}$/;
