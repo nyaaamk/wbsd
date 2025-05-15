3211527c0bd459b237d8ed4e7b3185639a041a21
cofinity01 <cofinity0@gmail.com>
2025-05-15 23:08:08 +0800

    updates: on cash-register

--- src/utils/cash-register.ts
+++ src/utils/cash-register.ts
@@ -1,199 +1,64 @@
-const _PAPERS = [
-  ["ONE HUNDRED", 100],
-  ["TWENTY", 20],
-  ["TEN", 10],
-  ["FIVE", 5],
-  ["ONE", 1],
-  ["QUARTER", 0.25],
-  ["DIME", 0.1],
-  ["NICKEL", 0.05],
-  ["PENNY", 0.01]
-];
-
+/* eslint-disable prefer-const */
 const PAPERS: { [key: string]: number } = {
-  "ONE HUNDRED": 100,
-  "TWENTY"     : 20,
-  "TEN"        : 10,
-  "FIVE"       : 5,
-  "ONE"        : 1,
-  "QUARTER"    : 0.25,
-  "DIME"       : 0.1,
+  "PENNY"      : 0.01,
   "NICKEL"     : 0.05,
-  "PENNY"      : 0.01
+  "DIME"       : 0.1,
+  "QUARTER"    : 0.25,
+  "ONE"        : 1.0,
+  "FIVE"       : 5.0,
+  "TEN"        : 10.0,
+  "TWENTY"     : 20.0,
+  "ONE HUNDRED": 100.0
 };
 
-const papers = Object.keys(PAPERS).map((key) => ({
-  name : key,
-  value: PAPERS[key]
-}));
+type ICash = [string, number];
 
-type IChange = [string, number];
+function checkCashRegister (price: number, cash: number, cid: ICash[]){
+  let amountChange = cash - price;
+  const change = [] as ICash[];
+  let amountTotal = cid.reduce((acc, itr) => acc + itr[1], 0);
+  amountTotal = Math.round(amountTotal * 100) / 100, amountTotal;
 
-function checkCashRegister (price: number, cash: number, cid: [string, number][]){
-  let amount = cash - price;
-  console.log(">>> amount >>>", amount);
-  if (amount < 0) return {};
+  if (amountTotal < amountChange) {
+    return { status: "INSUFFICIENT_FUNDS", change };
+  } else if (amountTotal === amountChange) {
+    return { status: "CLOSED", change: cid };
+  }
 
-  const cashInDrawer = papers.filter((paper) => {
-    const found = cid.find((c) => c[0] === paper.name);
-    if (found) {
-      return {
-        name : paper.name,
-        value: found[1]
-      };
+  const CID = cid.reverse();
+
+  for (let [paper, amount] of CID) {
+    const paperValue = PAPERS[paper];
+    let amountReturn = 0;
+
+    while (amountChange >= paperValue && amount >= paperValue) {
+      amountReturn += paperValue;
+      amount -= paperValue;
+      amountChange -= paperValue;
+
+      amountChange = Math.round(amountChange * 100) / 100;
     }
-  });
-  console.log(">>> cashInDrawer >>>", cashInDrawer);
 
-  const change: IChange[] = [];
-
-  let needForChange: IChange[] = papers.map((p) => ([p.name, 0]));
-
-  cashInDrawer.forEach((cid) => {
-    while (cid.value > amount) {
-      needForChange = needForChange.map((nfc) => {
-        let nfcAmount = nfc[1];
-        if (nfc[0] === cid.name) {
-          console.log(">>> nfc[0] === cid.name >>>", cid.name, cid.value);
-          nfcAmount = nfcAmount + cid.value;
-          console.log([nfc[0], nfcAmount]);
-        }
-
-        return [nfc[0], nfcAmount];
-      });
-
-      amount -= cid.value;
-      console.log(">>> amount >>>", amount);
+    if (amountReturn > 0) {
+      change.push([paper, amountReturn]);
     }
-  });
+  }
 
-  needForChange = needForChange.filter((nfc) => nfc[1]);
+  if (amountChange > 0) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  }
 
-  console.log(">>> needForChange >>>", needForChange);
-
-  return change;
+  return { status: "OPEN", change };
 }
 
-console.log(
-  checkCashRegister(19.5, 20, [
-    ["PENNY", 1.01],
-    ["NICKEL", 2.05],
-    ["DIME", 3.1],
-    ["QUARTER", 4.25],
-    ["ONE", 90],
-    ["FIVE", 55],
-    ["TEN", 20],
-    ["TWENTY", 60],
-    ["ONE HUNDRED", 100]
-  ])
-);
-
-/**
-console.log(
-  checkCashRegister(19.5, 20, [
-    ["PENNY", 1.01],
-    ["NICKEL", 2.05],
-    ["DIME", 3.1],
-    ["QUARTER", 4.25],
-    ["ONE", 90],
-    ["FIVE", 55],
-    ["TEN", 20],
-    ["TWENTY", 60],
-    ["ONE HUNDRED", 100]
-  ]),
-  {
-    status: "OPEN",
-    change: [["QUARTER", 0.5]]
-  }
-);
-
-console.log(
-  checkCashRegister(3.26, 100, [
-    ["PENNY", 1.01],
-    ["NICKEL", 2.05],
-    ["DIME", 3.1],
-    ["QUARTER", 4.25],
-    ["ONE", 90],
-    ["FIVE", 55],
-    ["TEN", 20],
-    ["TWENTY", 60],
-    ["ONE HUNDRED", 100]
-  ]),
-  {
-    status: "OPEN",
-    change: [
-      ["TWENTY", 60],
-      ["TEN", 20],
-      ["FIVE", 15],
-      ["ONE", 1],
-      ["QUARTER", 0.5],
-      ["DIME", 0.2],
-      ["PENNY", 0.04]
-    ]
-  }
-);
-
-console.log(
-  checkCashRegister(19.5, 20, [
-    ["PENNY", 0.01],
-    ["NICKEL", 0],
-    ["DIME", 0],
-    ["QUARTER", 0],
-    ["ONE", 0],
-    ["FIVE", 0],
-    ["TEN", 0],
-    ["TWENTY", 0],
-    ["ONE HUNDRED", 0]
-  ]),
-  {
-    status: "INSUFFICIENT_FUNDS",
-    change: []
-  }
-);
-
-console.log(
-  checkCashRegister(19.5, 20, [
-    ["PENNY", 0.01],
-    ["NICKEL", 0],
-    ["DIME", 0],
-    ["QUARTER", 0],
-    ["ONE", 1],
-    ["FIVE", 0],
-    ["TEN", 0],
-    ["TWENTY", 0],
-    ["ONE HUNDRED", 0]
-  ]),
-  {
-    status: "INSUFFICIENT_FUNDS",
-    change: []
-  }
-);
-
-console.log(
-  checkCashRegister(19.5, 20, [
-    ["PENNY", 0.5],
-    ["NICKEL", 0],
-    ["DIME", 0],
-    ["QUARTER", 0],
-    ["ONE", 0],
-    ["FIVE", 0],
-    ["TEN", 0],
-    ["TWENTY", 0],
-    ["ONE HUNDRED", 0]
-  ]),
-  {
-    status: "CLOSED",
-    change: [
-      ["PENNY", 0.5],
-      ["NICKEL", 0],
-      ["DIME", 0],
-      ["QUARTER", 0],
-      ["ONE", 0],
-      ["FIVE", 0],
-      ["TEN", 0],
-      ["TWENTY", 0],
-      ["ONE HUNDRED", 0]
-    ]
-  }
-);
-*/
+console.log(checkCashRegister(3.26, 100, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+]));
